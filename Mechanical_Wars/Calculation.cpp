d154548057f8fd5548427ec623fd5f771d0f3e65
LPC1768_ <hama911p@docomo.ne.jp>
2016-12-06 00:46:28 +0900

    const 指定に変更

--- Mechanical_Wars/Calculation.cpp
+++ Mechanical_Wars/Calculation.cpp
@@ -13,9 +13,9 @@ Vec2 Calculation::calculateDeviation(Vec2 pos1, Vec2 pos2, double v1, Vec2 v2)
 double Calculation::calculateCollisionTime(Vec2 pos1, Vec2 pos2, double v1, Vec2 v2)
 {
 	if (v2.length() == 0) return pos2.distanceFrom(pos1) / v1;
-	double cosA = (pos1 - pos2).dot(v2) / ((pos1 - pos2).length()*v2.length());
-	double a = v1;
-	double b = v2.length();
+	const double cosA = (pos1 - pos2).dot(v2) / ((pos1 - pos2).length()*v2.length());
+	const double a = v1;
+	const double b = v2.length();
 	Println((pos1 - pos2).length() / ((2 * b*cosA + sqrt(4 * b*b*cosA*cosA - 4 * (b*b - a*a))) / 2));
 	return (pos1 - pos2).length()/((2 * b*cosA + sqrt(4 * b*b*cosA*cosA - 4 * (b*b - a*a))) / 2);
 }
